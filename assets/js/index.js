71d057eafc5bf0536209630105c62420154c6acf
chan-0912 <yadavchanchal679@gmail.com>
2022-01-27 14:24:43 +0530

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// for navbar
+let toggle_btn = document.querySelector("#toggle_btn");
+let menu_ul = document.querySelector(".menu ul");
+
+let toggle = false;
+toggle_btn.addEventListener("click", function () {
+  console.log(toggle);
+  if (toggle == false) {
+    this.style.transform = "rotate(180deg)";
+    toggle = true;
+  } else {
+    this.style.transform = "rotate(0deg)";
+    toggle = false;
+  }
+  menu_ul.classList.toggle("active_menu");
+});
